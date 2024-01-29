8caed930fb0913570cc58e49713898cd01723fca
Valerii Stepanenko <walleriystepanenko@gmail.com>
2024-01-29 23:04:25 +0200

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getDirname } from '../utils/getDirname';
+
 const create = async () => {
-    // Write your code here 
+    const folderPath = 'files';
+    const filePath = path.join(getDirname(import.meta.url), folderPath, 'fresh.txt');
+    const fileContent = 'I am fresh and young';
+
+    try {
+        await fs.writeFile(filePath, fileContent, { flag: "wx" });
+        console.log('File created successfully: fresh.txt');
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
