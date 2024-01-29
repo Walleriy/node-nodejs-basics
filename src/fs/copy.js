a376da64c8a374e6dce6c40497ac1a6f9f1fb2d7
Valerii Stepanenko <walleriystepanenko@gmail.com>
2024-01-29 23:17:42 +0200

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getDirname } from '../utils/getDirname';
+
+const sourcePath = path.join(getDirname(import.meta.url), 'files');
+const targetPath = path.join(getDirname(import.meta.url), 'files_copy');
 const copy = async () => {
+    try {
+        await fs.cp(sourcePath, targetPath, { recursive: true, force: false, errorOnExist: true })
+        console.log('Folder copied successfully');
+    } catch {
+        throw new Error('FS operation failed');
+    }
     // Write your code here 
 };
 
--- src/fs/create.js
+++ src/fs/create.js
@@ -2,15 +2,15 @@ import fs from 'fs/promises';
 import path from 'path';
 import { getDirname } from '../utils/getDirname';
 
+const folderPath = 'files';
+const fileName = 'fresh.txt';
+const filePath = path.join(getDirname(import.meta.url), folderPath, fileName);
+const fileContent = 'I am fresh and young';
 const create = async () => {
-    const folderPath = 'files';
-    const filePath = path.join(getDirname(import.meta.url), folderPath, 'fresh.txt');
-    const fileContent = 'I am fresh and young';
-
     try {
         await fs.writeFile(filePath, fileContent, { flag: "wx" });
-        console.log('File created successfully: fresh.txt');
-    } catch (error) {
+        console.log(`File created successfully: ${fileName}`);
+    } catch {
         throw new Error('FS operation failed');
     }
 };
